dd971095612ce7a76864c336b9f16a19f3bb9f11
Justin Huang <xinglongjh@gmail.com>
2021-06-18 15:47:04 -0400

    Missing TODO in prox_l1

--- R/AManPG/R/prox_l1.R
+++ R/AManPG/R/prox_l1.R
@@ -1,5 +1,7 @@
 prox_l1 <- function(b, lambda, r) {
   a <- abs(b) - lambda
+
+  # TODO: Check what the heck the difference between these is
   if (r < 15) act_set <- as.numeric(a > 0) else act_set = a > 0
   x_prox <- act_set * sign(b) * a
   inact_set <- a <= 0
