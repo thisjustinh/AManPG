4f94efc750c31b2e8e6b331ed702033c2fbe4d9f
benjoch <65567813+benjoch@users.noreply.github.com>
2021-06-21 20:43:28 -0400

    Fixed "non-comfortable arrays" error
    I tested the algorithm with this change on the demo it fixed the non-comfortable arrays error. I still need to verify the output of the function as compared to the MATLAB function.

--- R/AManPG/R/prox.l1.R
+++ R/AManPG/R/prox.l1.R
@@ -2,7 +2,7 @@ prox.l1 <- function(b, lambda, r) {
   #print(b)
   #print(lambda)
   #print(r)
-  a <- abs(b) - lambda
+  a <- abs(b) - as.vector(lambda)
 
   # TODO: Check what the heck the difference between these is
   if (r < 15) act_set <- as.numeric(a > 0) else act_set = a > 0
