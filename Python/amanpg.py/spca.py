Oops, wrong axis, but still failing to converge.

Python/amanpg.py/spca.py
@@ -207,7 +207,7 @@ def normalize(x):
     x -= np.mean(x, axis=1, keepdims=True)  # center
 
     # normalize rows using l2 norm
-    x /= LA.norm(x, axis=0, keepdims=True)
+    x /= LA.norm(x, axis=1, keepdims=True)
     return x
 
 
@@ -233,7 +233,7 @@ if __name__ == '__main__':
         a = normalize(a)
         _, _, v = LA.svd(a)
         x0 = v[:, 0:n]
-        # print(np.mean(a, axis=0), LA.norm(a, axis=0))
+        # print(np.mean(a, axis=1)[0], LA.norm(a, axis=1)[0])
         iter, f_amanpg, sparsity, timediff, x, y_man = spca_amanpg(a, mu, lamb, n, x0, x0, f_palm, verbose=True)
         print(f"{iter} iterations with final value {f_amanpg}, sparsity {sparsity}, timediff {timediff}.")
 